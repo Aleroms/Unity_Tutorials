9802822e610cc095cdc6d40b678fe6c6bb041fc9
Aleroms <santiagomorales333@yahoo.com>
2020-07-07 23:27:02 -0700

    Used RayCasts to see if bullets collided with other colliders

--- SciFiDemo/SciFiDemo/Assets/Game/Scripts/Player.cs
+++ SciFiDemo/SciFiDemo/Assets/Game/Scripts/Player.cs
@@ -7,18 +7,83 @@ public class Player : MonoBehaviour {
 	[SerializeField]
 	private float _speed = 5f;
 	private float _gravity = 9.81f;
+
+	private AudioSource _audioSource;
+
+	[SerializeField]
+	private GameObject _muzzleFire;
+	[SerializeField]
+	private GameObject _hitMarkerPrefab;
+
+	private int _currentAmmo;
+	private int _maxAmmo = 50;
+
+	private bool _isReloading = false;
+
+	private UIManager _uiManager;
 	// Use this for initialization
 	void Start () {
+
+		_uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+
+		if (_uiManager == null)
+			Debug.LogError("UIManager is null");
+
+		_currentAmmo = _maxAmmo;
+
 		Cursor.lockState = CursorLockMode.Locked;
 		_controller = GetComponent<CharacterController>();
+
+		_audioSource = GetComponent<AudioSource>();
+
+		if (_audioSource == null)
+			Debug.LogError("AudioSource is null");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		CalculateMovement();
+
+		if (Input.GetMouseButton(0) && _currentAmmo > 0)
+		{
+			_currentAmmo--;
+			Shoot();
+			_uiManager.UpdateAmmoCount(_currentAmmo);
+		}
+		else
+		{
+			_muzzleFire.SetActive(false);
+			_audioSource.Stop();
+		}
+
+		if (Input.GetKeyDown(KeyCode.R) && !_isReloading)
+		{
+			_isReloading = true;
+			StartCoroutine(Reload());
+		}
 
 		if (Input.GetKeyDown(KeyCode.Escape))
 			Cursor.lockState = CursorLockMode.None;
+
+		CalculateMovement();
+	}
+	void Shoot()
+	{
+
+		_muzzleFire.SetActive(true);
+
+		Ray rayOrigin = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+		RaycastHit hitInfo;
+
+		if (Physics.Raycast(rayOrigin, out hitInfo, Mathf.Infinity))
+		{
+			GameObject hit = Instantiate(_hitMarkerPrefab, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
+			Destroy(hit, 0.2f);
+
+		}
+		if (!_audioSource.isPlaying)
+			_audioSource.Play();
+
+		print(hitInfo.transform.name);
 	}
 	void CalculateMovement()
 	{
@@ -30,5 +95,12 @@ public class Player : MonoBehaviour {
 
 		velocity = transform.TransformDirection(velocity);
 		_controller.Move(velocity * Time.deltaTime * _speed);
+	}
+	IEnumerator Reload()
+	{
+		yield return new WaitForSeconds(1.5f);
+		_currentAmmo = _maxAmmo;
+		_uiManager.UpdateAmmoCount(_currentAmmo);
+		_isReloading = false;
 	}
 }
---
+++ SciFiDemo/SciFiDemo/Assets/Game/Scripts/UIManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+	[SerializeField]
+	private Text _ammoText; 
+
+	void Start()
+	{
+		_ammoText.text = "Ammo: 50";
+	}
+	public void UpdateAmmoCount(int n)
+	{
+		_ammoText.text = "Ammo: " + n;
+	}
+}
